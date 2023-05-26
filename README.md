3d9e87020378baf9ac05e4c852d25658b7173d12
宏 <977106024@qq.com>
2023-05-26 16:26:45 +0800

    up

---
+++ README.md
@@ -0,0 +1,5 @@
+上班偷偷看大A  隐藏到命令行当中
+
+npm i assaulta -g   
+安装后在命令行中输入upa  
+upa 后面可跟代码参数 如 upa SZ30059
--- index.js
+++ index.js
@@ -4,46 +4,90 @@
 const axios = require('axios');
 const colors = require('colors')
 
-const list = {
-  '首':'SH601136',
-  '-': 'SZ002594',
-  '比': 'SZ300750',
-  '*': 'SZ002049'
-}
+const list = [
+  'SH601136',
+  'SH600072',
+  'SH512880'
+]
+const nameArr = ['S','E','中']
 
-const errorList = ['Maximum call stack size exceeded','"god" is not defined','Invalid or unexpected token','Cannot read property "node" of undefined']
+const errorList = ['Maximum call stack size exceeded', '"god" is not defined', 'Invalid or unexpected token', 'Cannot read property "node" of undefined']
 
-const query = process.argv[2] ? process.argv[2] : Object.values(list).toString()
+const query = process.argv[2] ? process.argv[2] : list.toString()
 const url = `https://stock.xueqiu.com/v5/stock/realtime/quotec.json?symbol=${query}`
 
 
 axios.get(url)
   .then(res => {
     const data = res.data.data
-    data.forEach((item,i) => {
-      const {low,high} = item
+    data.forEach((item, i) => {
+      const { low, high } = item
       let current = ''
       let percent = ''
-      let name = Object.keys(list)[i]
-      if(String(item.percent).indexOf('-') === -1){
+      let name = nameArr[i]
+      if (String(item.percent).indexOf('-') === -1) {
         current = colors.red.bold(item.current)
         percent = colors.red(item.percent)
-      }else{
+      } else {
         current = colors.green.bold(item.current)
-        const i = Math.floor(Math.random()* errorList.length)
-        console.error(errorList[i])
+        const i = Math.floor(Math.random() * errorList.length)
+        console.trace(errorList[i])
         percent = colors.green(item.percent)
       }
-      if(name == '*'){
+      if (name == '*') {
         name = colors.magenta(name)
       }
-        console.log(name,current,percent,    low);
+      console.log(name, current, percent, low);
     });
-    
+
   })
   .catch(err => {
-    console.log(err,'sss');
+    console.log(err, 'sss');
   });
 
+const XNY = [
+  'SZ002594',
+  'SZ300750',
+  'SZ300274',
+  'SH601012'
+]
+
+const url2 = `https://stock.xueqiu.com/v5/stock/realtime/quotec.json?symbol=${XNY.toString()}`
+
+axios.get(url2)
+  .then(res => {
+    const data = res.data.data
+    const arr = data.map((item, i) => {
+      const { current, percent } = item
+      return [current, percent]
+    });
+    console.trace(arr);
 
 
+  })
+  .catch(err => {
+    console.log(err, 'sss');
+  });
+
+  const AI = [
+    'SH603019',
+    'SH603083',
+    'SH603888'
+  ]
+
+  const urlAI = `https://stock.xueqiu.com/v5/stock/realtime/quotec.json?symbol=${AI.toString()}`
+
+axios.get(urlAI)
+  .then(res => {
+    const data = res.data.data
+    const arr = data.map((item, i) => {
+      const { current, percent, low } = item
+      return [current, percent, low]
+    });
+    console.trace(arr);
+
+
+  })
+  .catch(err => {
+    console.log(err, 'sss');
+  });
